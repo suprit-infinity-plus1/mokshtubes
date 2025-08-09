819cb0739a39b3f42b08ef988b2ff7abf220f679
Zaid-Shaikh-06 <skmdza8d@gmail.com>
2025-08-09 18:02:02 +0530

    add meta title and description and blog page add

--- resources/views/frontend/blogs.blade.php
+++ resources/views/frontend/blogs.blade.php
@@ -69,6 +69,36 @@
                     <div class="default-blog-news wow fadeInUp animated animated"
                         style="visibility: visible; animation-name: fadeInUp;">
                         <figure class="img-holder mb-0">
+                            <a href="#"><img src="{{ asset('assets/images/blogs/Inconel-vs-Stainless-Steel.jpg') }}" alt="News"></a>
+                            <figcaption class="overlay">
+                                <div class="box">
+                                    <div class="content">
+                                        <a href="#"><i class="fa fa-link" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure>
+                        <div class="lower-content">
+                            <a href="#">
+                                <h5>MATERIALS & MANUFACTURING </h5>
+                            </a>
+                            <div class="post-meta">by fletcher | 14 Comments | 21 Feb</div>
+                            <div class="text">
+                                <p>Except to obtain some advantage from it? But who has any right to find to enjoy a
+                                    pleasure.</p>
+                            </div>
+                            <div class="link">
+                                <a href="#" class="default_link">Read More <i class="fa fa-angle-right"></i></a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="default-blog-news wow fadeInUp animated animated"
+                        style="visibility: visible; animation-name: fadeInUp;">
+                        <figure class="img-holder mb-0">
                             <a href="#"><img src="{{ asset('assets/images/blog/blog.png') }}" alt="News"></a>
                             <figcaption class="overlay">
                                 <div class="box">
